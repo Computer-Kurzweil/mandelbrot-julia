9734886f55d92f4c4900f37e656406d73592a188
thomaswoehlke <thomas.woehlke@gmail.com>
2022-12-16 15:34:36 +0100

    work

--- src/main/java/org/woehlke/computer/kurzweil/mandelbrot/julia/model/fractal/GaussianNumberPlane.java
+++ src/main/java/org/woehlke/computer/kurzweil/mandelbrot/julia/model/fractal/GaussianNumberPlane.java
@@ -36,23 +36,11 @@ public class GaussianNumberPlane {
     private final static double complexCenterForMandelbrotRealX = -2.2f;
     private final static double complexCenterForMandelbrotImgY = -1.17f;
     private final static double complexCenterForJuliaRealX = -1.6d;
-    private final static double complexCenterForJuliaImgY =  -1.17d;
+    private final static double complexCenterForJuliaImgY = -1.17d;
 
     private volatile ComplexNumber complexWorldDimensions;
     private volatile ComplexNumber complexCenterForMandelbrot;
     private volatile ComplexNumber complexCenterForJulia;
-
-    @Deprecated
-    public volatile int zoomLevel;
-
-    @Deprecated
-    private volatile Deque<ComplexNumber> complexCenterForZoomedMandelbrot = new ArrayDeque<>();
-
-    @Deprecated
-    private volatile ComplexNumber zoomCenter;
-
-
-    //public static Logger log = Logger.getLogger(GaussianNumberPlane.class.getName());
 
     public GaussianNumberPlane(ApplicationModel model) {
         this.worldDimensions = model.getWorldDimensions();
@@ -72,69 +60,38 @@ public class GaussianNumberPlane {
         start();
     }
 
-    @Deprecated
-    public void setModeZoom() {
-        this.setZoomLevel(1);
-        this.setZoomCenter(complexCenterForMandelbrot);
-    }
-
-    public synchronized void start(){
-        zoomLevel = 1;
-        for(int y = 0;y < this.worldDimensions.getY(); y++){
-            for(int x=0; x < worldDimensions.getX(); x++){
+    public synchronized void start() {
+        for (int y = 0; y < this.worldDimensions.getY(); y++) {
+            for (int x = 0; x < worldDimensions.getX(); x++) {
                 lattice[x][y] = YET_UNCOMPUTED;
             }
         }
     }
 
-    public synchronized int getCellStatusFor(int x,int y){
-        return (lattice[x][y])<0?0:lattice[x][y];
+    public synchronized int getCellStatusFor(int x, int y) {
+        return (lattice[x][y]) < 0 ? 0 : lattice[x][y];
     }
 
     private synchronized ComplexNumber getComplexNumberFromLatticeCoordsForJulia(Point turingPosition) {
         double realX = complexCenterForJulia.getReal()
-            + (complexWorldDimensions.getReal()*turingPosition.getX())/worldDimensions.getX();
+            + (complexWorldDimensions.getReal() * turingPosition.getX()) / worldDimensions.getX();
         double imgY = complexCenterForJulia.getImg()
-            + (complexWorldDimensions.getImg()*turingPosition.getY())/worldDimensions.getY();
-        return new ComplexNumber(realX,imgY);
+            + (complexWorldDimensions.getImg() * turingPosition.getY()) / worldDimensions.getY();
+        return new ComplexNumber(realX, imgY);
     }
 
     private synchronized ComplexNumber getComplexNumberFromLatticeCoordsForMandelbrot(Point turingPosition) {
         double realX = (
             complexCenterForMandelbrot.getReal()
-            + ( complexWorldDimensions.getReal() * turingPosition.getX() )
-            / worldDimensions.getX()
+                + (complexWorldDimensions.getReal() * turingPosition.getX())
+                / worldDimensions.getX()
         );
         double imgY = (
             complexCenterForMandelbrot.getImg()
-            + ( complexWorldDimensions.getImg() * turingPosition.getY() )
-            / worldDimensions.getY()
+                + (complexWorldDimensions.getImg() * turingPosition.getY())
+                / worldDimensions.getY()
         );
-        return new ComplexNumber(realX,imgY);
-    }
-
-    @Deprecated
-    private synchronized ComplexNumber getComplexNumberFromLatticeCoordsForZoomedMandelbrot(Point turingPosition) {
-        double realX = (
-            ( complexCenterForMandelbrot.getReal() / this.getZoomLevel() )
-            + getZoomCenter().getReal()
-            + ( complexWorldDimensions.getReal() * turingPosition.getX() )
-            / ( worldDimensions.getX() * this.getZoomLevel() )
-        );
-        double imgY = (
-            ( complexCenterForMandelbrot.getImg() / this.getZoomLevel() )
-            + getZoomCenter().getImg()
-            + ( complexWorldDimensions.getImg() * turingPosition.getY() )
-            / ( worldDimensions.getY() * this.getZoomLevel() )
-        );
-        return new ComplexNumber(realX,imgY);
-    }
-
-    @Deprecated
-    public synchronized boolean isInZooomedMandelbrotSet(Point turingPosition) {
-        ComplexNumber position = this.getComplexNumberFromLatticeCoordsForZoomedMandelbrot(turingPosition);
-        lattice[turingPosition.getX()][turingPosition.getY()] = position.computeMandelbrotSet();
-        return position.isInMandelbrotSet();
+        return new ComplexNumber(realX, imgY);
     }
 
     public synchronized boolean isInMandelbrotSet(Point turingPosition) {
@@ -143,10 +100,10 @@ public class GaussianNumberPlane {
         return position.isInMandelbrotSet();
     }
 
-    public synchronized void fillTheOutsideWithColors(){
-        for(int y=0;y<worldDimensions.getY();y++){
-            for(int x=0;x<worldDimensions.getX();x++){
-                if(lattice[x][y] == YET_UNCOMPUTED){
+    public synchronized void fillTheOutsideWithColors() {
+        for (int y = 0; y < worldDimensions.getY(); y++) {
+            for (int x = 0; x < worldDimensions.getX(); x++) {
+                if (lattice[x][y] == YET_UNCOMPUTED) {
                     this.isInMandelbrotSet(new Point(x, y));
                 }
             }
@@ -154,7 +111,7 @@ public class GaussianNumberPlane {
     }
 
     private synchronized void computeTheJuliaSetForC(ComplexNumber c) {
-        for(int y = 0; y < worldDimensions.getY(); y++) {
+        for (int y = 0; y < worldDimensions.getY(); y++) {
             for (int x = 0; x < worldDimensions.getX(); x++) {
                 Point zPoint = new Point(x, y);
                 ComplexNumber z = this.getComplexNumberFromLatticeCoordsForJulia(zPoint);
@@ -168,83 +125,4 @@ public class GaussianNumberPlane {
         this.complexNumberForJuliaSetC = c;
         computeTheJuliaSetForC(c);
     }
-
-    @Deprecated
-    public void zoomIntoTheMandelbrotSet(Point zoomPoint) {
-        //log.info("zoomIntoTheMandelbrotSet: "+ zoomPoint +" - old:  "+this.getZoomCenter());
-        this.inceaseZoomLevel();
-        if(this.getZoomLevel() == 2){
-            ComplexNumber complexCenter = new ComplexNumber(this.complexCenterForMandelbrot);
-            complexCenterForZoomedMandelbrot.push(complexCenter);
-            this.setZoomCenter(getComplexNumberFromLatticeCoordsForMandelbrot(zoomPoint));
-        } else {
-            this.setZoomCenter(getComplexNumberFromLatticeCoordsForZoomedMandelbrot(zoomPoint));
-        }
-        complexCenterForZoomedMandelbrot.push(this.getZoomCenter());
-        //log.info("zoomPoint:     "+ zoomPoint);
-        //log.info("zoomCenterNew: " + this.getZoomCenter() + " - zoomLevel:  "+ this.getZoomLevel());
-        for(int y = 0; y < worldDimensions.getY(); y++){
-            for(int x = 0; x < worldDimensions.getX(); x++){
-                Point p = new Point(x, y);
-                this.isInZooomedMandelbrotSet(p);
-            }
-        }
-    }
-
-    @Deprecated
-    public void zoomOutOfTheMandelbrotSet() {
-        //log.info("zoomOutOfTheMandelbrotSet: " + this.getZoomCenter());
-        if(this.getZoomLevel()>1){
-            this.deceaseZoomLevel();
-            this.setZoomCenter(complexCenterForZoomedMandelbrot.pop());
-        }
-        //log.info("zoomCenter: " + this.getZoomCenter() + " - zoomLevel:  "+ this.getZoomLevel());
-        for(int y = 0; y < worldDimensions.getY(); y++){
-            for(int x = 0; x < worldDimensions.getX(); x++){
-                Point p = new Point(x, y);
-                this.isInZooomedMandelbrotSet(p);
-            }
-        }
-    }
-
-    @Deprecated
-    public void zoomIntoTheJuliaSetFor(Point zoomPoint) {
-        ComplexNumber c = this.complexNumberForJuliaSetC;
-        computeTheJuliaSetForC(c);
-    }
-
-    @Deprecated
-    public void zoomOutOfTheJuliaSet() {
-    }
-
-    @Deprecated
-    public synchronized int getZoomLevel() {
-        return zoomLevel;
-    }
-
-    @Deprecated
-    public synchronized int inceaseZoomLevel() {
-        return zoomLevel *= 2;
-    }
-
-    @Deprecated
-    public synchronized int deceaseZoomLevel() {
-        return zoomLevel /= 2;
-    }
-
-    @Deprecated
-    public synchronized void setZoomLevel(int zoomLevel) {
-        this.zoomLevel = zoomLevel;
-    }
-
-    @Deprecated
-    public synchronized ComplexNumber getZoomCenter() {
-        return zoomCenter;
-    }
-
-    @Deprecated
-    public synchronized void setZoomCenter(ComplexNumber zoomCenter) {
-        this.zoomCenter = zoomCenter;
-    }
-
 }
